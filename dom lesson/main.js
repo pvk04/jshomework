365ca04478fb47781aef0aa49155d551d2e9cb17
Alex <97796170+pvk04@users.noreply.github.com>
2022-02-23 20:04:44 +0300

    Add files via upload

--- dom lesson/main.js
+++ dom lesson/main.js
@@ -1,11 +1,4 @@
-testArray = [
-    { value: '1', label: '321' },
-    { value: '2', label: '4321' },
-    { value: '3', label: '54321' },
-    { value: '4', label: '654321' },
-    { value: 'gg', label: 'og' }
-]
-
+// ex 1
 function initialisatorOpt(array, value = array[0].value) {
     let select = document.createElement("select")
 
@@ -31,5 +24,53 @@ function initialisatorOpt(array, value = array[0].value) {
     return select
 }
 
+//test 1
+testArray = [
+    { value: '1', label: '321' },
+    { value: '2', label: '4321' },
+    { value: '3', label: '54321' },
+    { value: '4', label: '654321' },
+    { value: 'gg', label: 'og' }
+]
+
 let selectReady = initialisatorOpt(testArray, '4')
 document.body.append(selectReady)
+
+
+
+
+// ex 2
+function sortOptData(data) {
+
+    let resultArray = []
+
+    //если в функцию посылается массив
+    if (Array.isArray(data)) {
+        for (i of data) {
+            obj = { 'value': i, 'label': i }
+            resultArray.push(obj)
+        }
+        return resultArray
+    }
+
+    //если в функцию посылается объект
+    for (key in data) {
+        obj = { 'value': data[key], 'label': data[key] }
+        resultArray.push(obj)
+    }
+    return resultArray
+}
+
+
+// test array
+let testArr = [1, 2, 'три', 'четыре']
+
+let selectReady21 = initialisatorOpt(sortOptData(testArr), 'четыре')
+document.body.append(selectReady21)
+
+//test object
+let testObj = { value1: 'значение1', valuel: 'значение 2', abc: 123 }
+
+let selectReady22 = initialisatorOpt(sortOptData(testObj), 123)
+document.body.append(selectReady22)
+
