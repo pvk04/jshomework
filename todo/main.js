1a634b05660d35e64c1a6a77a49d98871260397e
Alex <97796170+pvk04@users.noreply.github.com>
2022-03-24 14:40:42 +0300

    Add files via upload

--- todo/main.js
+++ todo/main.js
@@ -4,31 +4,30 @@
         title.classList = "apptitle";
         title.setAttribute("id", "title");
         title.textContent = text;
-
+  
         return title;
     }
-
+  
     function createTodoItemForm() {
         const divItems = document.createElement("div");
         divItems.classList = "todoitemform";
         divItems.setAttribute("id", "createform");
-
+  
         const form = document.createElement("form");
         form.classList = "form";
         divItems.append(form);
-
+  
         const input = document.createElement("input");
         input.setAttribute("placeholder", "Введите новое дело");
         input.classList = "inp";
         input.setAttribute("id", "input1");
         form.append(input);
-
+  
         const buttonDone = document.createElement("button");
-        buttonDone.setAttribute("type", "button");
         buttonDone.classList = "inputbutton";
         buttonDone.innerHTML = "Добавить";
         form.append(buttonDone);
-
+  
         return {
             divItems,
             form,
@@ -36,65 +35,66 @@
             buttonDone
         }
     }
-
+  
     function createTodoList() {
         const ul = document.createElement("ul");
         ul.setAttribute("id", "todolist");
         ul.classList = "list";
-
+  
         return ul;
     }
-
+  
     function createTodoElement(textContent) {
         const li = document.createElement("li");
         li.setAttribute("id", "li");
-
+  
         const div = document.createElement("div");
         div.classList = "liDiv";
         li.append(div);
-
+  
         let text = document.createElement("p");
         text.classList = "liText";
         text.textContent = textContent;
         div.append(text);
-
+  
         const buttonDiv = document.createElement("div");
         buttonDiv.classList = "liButtons";
         div.append(buttonDiv);
-
+  
         const buttonReady = document.createElement("button");
         buttonReady.textContent = "Готово";
         buttonReady.classList = "liReadyButton";
         buttonDiv.append(buttonReady);
-
+  
         const buttonDel = document.createElement("button");
         buttonDel.textContent = "Удалить";
         buttonDel.classList = "liReadyButton";
         buttonDiv.append(buttonDel);
-
+  
         return {
             li,
             text,
             buttonReady,
             buttonDel
         }
-
+  
     }
-
-
-    document.addEventListener("DOMContentLoaded", () => {
+  
+  
+    function createTodo(name) {
         const todo = document.querySelector(".todo");
-
-        let title = createAppTitle("Список дел");
+  
+        let title = createAppTitle(name);
         todo.append(title);
-
+  
         let itemForm = createTodoItemForm();
         todo.append(itemForm.divItems);
-
+  
         let ul = createTodoList();
         todo.append(ul);
-
-        itemForm.buttonDone.addEventListener("click", () => {
+  
+        itemForm.form.addEventListener("submit", (e) => {
+            e.preventDefault();
             if (itemForm.input.value != "") {
                 let list = createTodoElement(itemForm.input.value);
                 list.buttonReady.addEventListener("click", () => {
@@ -106,26 +106,25 @@
                         list.li.style.textDecoration = "line-through";
                     }
                 })
-
+  
                 list.buttonDel.addEventListener("click", () => {
                     let conf = confirm("Вы действительно хотите удалить эту задачу?");
                     if (conf) {
                         list.li.remove();
                     }
                 })
-
+  
                 ul.append(list.li);
             }
             else {
                 alert("Поле ввода не может быть пустым!");
             }
-
+  
             itemForm.input.value = "";
         })
-    })
-
-
-
-
-
-})();
+    }
+  
+  window.createTodo = createTodo;
+  
+  
+  })();
