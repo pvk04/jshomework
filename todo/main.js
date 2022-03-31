5819695418b29f6edf9a3093cf4e77d69afaa3c5
Alex <97796170+pvk04@users.noreply.github.com>
2022-03-31 14:55:40 +0300

    Add files via upload

--- todo/main.js
+++ todo/main.js
@@ -1,8 +1,8 @@
 (function () {
 
     let todoArray = [
-        {id: 0, name: "Помыть посуду", done: true},
-        {id: 1, name: "Выгулять собаку", done: false}
+        { id: 0, name: "Помыть посуду", done: true },
+        { id: 1, name: "Выгулять собаку", done: false }
     ];
 
     function createAppTitle(text) {
@@ -10,30 +10,30 @@
         title.classList = "apptitle";
         title.setAttribute("id", "title");
         title.textContent = text;
-  
+
         return title;
     }
-  
+
     function createTodoItemForm() {
         const divItems = document.createElement("div");
         divItems.classList = "todoitemform";
         divItems.setAttribute("id", "createform");
-  
+
         const form = document.createElement("form");
         form.classList = "form";
         divItems.append(form);
-  
+
         const input = document.createElement("input");
         input.setAttribute("placeholder", "Введите новое дело");
         input.classList = "inp";
         input.setAttribute("id", "input1");
         form.append(input);
-  
+
         const buttonDone = document.createElement("button");
         buttonDone.classList = "inputbutton";
         buttonDone.innerHTML = "Добавить";
         form.append(buttonDone);
-  
+
         return {
             divItems,
             form,
@@ -41,43 +41,43 @@
             buttonDone
         }
     }
-  
+
     function createTodoList() {
         const ul = document.createElement("ul");
         ul.setAttribute("id", "todolist");
         ul.classList = "list";
-  
+
         return ul;
     }
-  
-    function createTodoElement({id, name, done}) {
+
+    function createTodoElement({ id, name, done }) {
         const li = document.createElement("li");
         li.setAttribute("id", "li");
-  
+
         const div = document.createElement("div");
         div.classList = "liDiv";
         li.append(div);
-  
+
         let text = document.createElement("p");
         text.classList = "liText";
         text.textContent = name;
         div.append(text);
-  
+
         const buttonDiv = document.createElement("div");
         buttonDiv.classList = "liButtons";
         div.append(buttonDiv);
-  
+
         const buttonReady = document.createElement("button");
         buttonReady.textContent = "Готово";
         buttonReady.classList = "liReadyButton";
         buttonDiv.append(buttonReady);
-  
+
         const buttonDel = document.createElement("button");
         buttonDel.textContent = "Удалить";
         buttonDel.classList = "liReadyButton";
         buttonDiv.append(buttonDel);
 
-        if (done){
+        if (done) {
             li.classList.add("item");
         }
 
@@ -89,10 +89,10 @@
             buttonReady,
             buttonDel
         }
-  
+
     }
-  
-  
+
+
     function createTodo(nameApp, array = [], key) {
 
         //
@@ -102,13 +102,13 @@
 
 
         const todo = document.querySelector(".todo");
-  
+
         let title = createAppTitle(nameApp);
         todo.append(title);
-  
+
         let itemForm = createTodoItemForm();
         todo.append(itemForm.divItems);
-  
+
         let ul = createTodoList();
         todo.append(ul);
 
@@ -117,45 +117,68 @@
         itemForm.buttonDone.setAttribute("disabled", "disabled");
         itemForm.input.addEventListener("input", () => {
             itemForm.buttonDone.removeAttribute("disabled");
-            if (itemForm.input.value == ""){
+            if (itemForm.input.value == "") {
                 itemForm.buttonDone.setAttribute("disabled", "disabled");
             }
         });
-  
+
 
         itemForm.form.addEventListener("submit", (e) => {
             e.preventDefault();
 
             if (itemForm.input.value != "") {
-                let list = createTodoElement({name: itemForm.input.value, done: false});
+                let item = { id: array.length, name: itemForm.input.value, done: false };
+                let list = createTodoElement(item);
 
                 // localstore array pushing
-                let item = {id: array.length, name: itemForm.input.value, done: false};
                 array.push(item);
                 localStorage.setItem(key, JSON.stringify(array));
-                //
 
 
                 list.buttonReady.addEventListener("click", () => {
                     list.li.classList.toggle("item");
                     // local store array ready
-                    
 
-                    
-
+                    // let newArr = array.filter((arr) => {
+                    //     item.id != arr.id
+                    //     if (item.id == arr.id) {
+                    //         if (arr.done == true) {
+                    //             arr.done = false;
+                    //         }
+                    //         else {
+                    //             arr.done = true;
+                    //         }
+                    //     }
+                    // });
+                    let newArr = [];
+                    for (let i = 0; i < array.length; i++) {
+                        if (item.id != array[i].id) {
+                            newArr.push(array[i]);
+                        }
+                        else {
+                            if (array[i].done) {
+                                array[i].done = false;
+                            }
+                            else {
+                                array[i].done = true;
+                            }
+                            newArr.push(array[i]);
+                        }
+                    }
+                    localStorage.setItem(key, JSON.stringify(newArr));
                 });
-  
+
                 list.buttonDel.addEventListener("click", () => {
                     let conf = confirm("Вы действительно хотите удалить эту задачу?");
                     if (conf) {
                         // local store item remove
-                        newArr = array.filter((arr) => item.id != arr.id);
+                        let newArr = array.filter((arr) => item.id != arr.id);
                         localStorage.setItem(key, JSON.stringify(newArr));
 
                         list.li.remove();
                     }
                 });
-                
+
 
                 ul.append(list.li);
 
@@ -166,32 +189,39 @@
             else {
                 alert("Поле ввода не может быть пустым!");
             }
-  
+
             itemForm.input.value = "";
         });
 
         // array reader
-        for (let i = 0; i<array.length; i++){
+        for (let i = 0; i < array.length; i++) {
             let elem = createTodoElement(array[i]);
 
             elem.buttonReady.addEventListener("click", () => {
-                if (elem.li.style.backgroundColor == "rgb(102, 212, 85)") {
-                    elem.li.classList.toggle("item");
-                } else {
-                    elem.li.classList.toggle("item");
+                elem.li.classList.toggle("item");
+
+                if (array[i].done) {
+                    array[i].done = false;
                 }
+                else {
+                    array[i].done = true;
+                }
+                localStorage.setItem(key, JSON.stringify(array));
             });
             elem.buttonDel.addEventListener("click", () => {
                 let conf = confirm("Вы действительно хотите удалить эту задачу?");
                 if (conf) {
+                    let newArr = array.filter((arr) => array[i].id != arr.id);
+                    localStorage.setItem(key, JSON.stringify(newArr));
+
                     elem.li.remove();
                 }
             });
             ul.append(elem.li);
         }
     }
-  
-  window.createTodo = createTodo;
-  window.todoArray = todoArray;
 
-  })();
+    window.createTodo = createTodo;
+    window.todoArray = todoArray;
+
+})();
