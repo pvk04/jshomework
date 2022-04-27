9fa53ae82fe6328f62c89a09183b31d1f829c5ad
Alex <97796170+pvk04@users.noreply.github.com>
2022-04-27 20:49:24 +0300

    Add files via upload

--- input auto correct/main.js
+++ input auto correct/main.js
@@ -20,4 +20,32 @@
 
         return button;
     }
+
+    function createApp(){
+        let body = document.querySelector(".inputSub");
+        let wordsArray = ["Russia", "Germany", "Sweden", "Rambler", "Rosses", "rock", "Rucola"];
+
+        let input = createInput();
+        let inputDiv = createSubDiv();
+
+        body.append(input);
+        body.append(inputDiv);
+
+        input.addEventListener("input", () => {
+            for (let i = 0; i < wordsArray.length; i++){
+                if (wordsArray[i].startsWith(input.value)){
+                    let button = createSubstitution();
+                    button.textContent = wordsArray[i];
+
+                    button.addEventListener("click", () => {
+                        input.value = button.textContent;
+                    });
+
+                    inputDiv.append(button);
+                }
+            }
+        });
+    }
+
+    createApp();
 }())
